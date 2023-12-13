a7f8405f8317712c84ec32c4e289e96a641a42f6
chrisnastro <cnastro@icloud.com>
2023-12-12 23:20:47 -0500

    coded timer

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,21 @@ var questionBox = document.getElementById("questionbox");
 var finish = document.getElementById("finish");
 var scoreButton = document.getElementById("scorebutton");
 var initials = document.getElementById("initials");
+var queCount = 0;
+var counter = 60;
+
+startButton.onclick = () => {
+    function countdown() {
+        counter--;
+        if (counter === 0) {
+            clearInterval(startCountdown)
+            timeUp()
+        };
+        let timeRemaining = document.queryselector("#timeleft");
+        let timeTag = "<span>Time Remaining: " + counter + "</span>"
+        timeRemaining.innerHtml = timeTag;
+    };
+    var startTimer = setInterval(countdown, 1000);
+    questionBox.classList.remove("hide");
+    showQuestions(queCount)
+};
